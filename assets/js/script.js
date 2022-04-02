43a955e91711c4bf1accbc3ec384d610ec5c40bd
julamifra <julamifra@gmail.com>
2022-04-02 22:54:27 +0000

    Add subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,9 +28,9 @@ function runGame(gameType){
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if(gameType === "addition") {
-        displayAdditionQuestion(num1, num2)
+        displayAdditionQuestion(num1, num2);
     } else if(gameType === "subtract"){
-
+        displaySubtractQuestion(num1, num2);
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "division"){
@@ -70,10 +70,9 @@ function calculateCorrectAnswer(){
     if(operator === "+"){
         return [operand1 + operand2, "addition"];
     } else if(operator === "X"){
-        return [operand1 + operand2, "addition"];
-
+        return [operand1 * operand2, "addition"];
     } else if(operator === "-"){
-
+        return [operand1 - operand2, "addition"];
     } else if (operator === "/"){
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator}`
@@ -104,7 +103,10 @@ function displayAdditionQuestion(operand1, operand2){
 
 }
 
-function displaySubtractQuestion(){
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
